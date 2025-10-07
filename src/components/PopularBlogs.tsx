4fc99dc7ba72010cefb767f32aff94261093ab44
Moses Valladares <mvallad1@binghamton.edu>
2025-10-06 22:09:53 -0400

    completed product pages for all products with the ability to return to main store page

---
+++ src/components/PopularBlogs.tsx
@@ -0,0 +1,10 @@
+
+const PopularBlogs = () => {
+  return (
+    <div>
+        PopularBlogs
+    </div>
+  )
+}
+
+export default PopularBlogs
--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -36,6 +36,16 @@ const ProductPage = () => {
         <button onClick={() => navigate(-1)} className="mb-5 px-4 py-2 bg-black text-white rounded">
            Back 
         </button>
+
+        <img src={product.images[0]} alt={product.title} className="w-[50%] h-auto mb-5" />
+        <h1 className="text-2xl mb-4 font-bold">
+          {product.title} 
+        </h1>
+        <p className=" text-gray-700 w-[70%] ml-3 mb-4">{product.description}</p>
+        <div className="flex">
+          <p>Price: ${product.price}</p>
+          <p className="ml-10">Rating: {product.rating}</p>
+        </div>
     </div>
   )
 }
