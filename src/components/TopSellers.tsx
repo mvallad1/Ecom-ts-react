f1c992019e310dc89a06eca6ed87173891458eb8
Moses Valladares <mvallad1@binghamton.edu>
2025-10-09 22:10:17 -0400

    finished the top sellers section and added the ability to follow and unfollow

--- src/components/TopSellers.tsx
+++ src/components/TopSellers.tsx
@@ -30,9 +30,32 @@ const TopSellers = () => {
     fetchData();
   }, []);
 
+  const handleFollowClick = (index: number) => {
+    setAuthors(prevAuthor => prevAuthor.map((author, i) => (
+      i === index ? {...author, isFollowing: !author.isFollowing} : author
+    )));
+  };
+
   return (
     <div className="bg-white p-5 mx-5 mt-[5rem] border w-[23rem] rounded">
         <h2 className="text-xl font-bold mb-2">Top Sellers</h2>
+
+        <ul>
+          {authors.map((author, index) => (
+            <li key={index} className="flex items-center justify-between mb-4">
+              <section className="flex justify-center items-center">
+                <img src={author.image} alt={author.name} className="w-[25%] h-[25%] justify-center rounded-full" />
+                <span className="ml-4">{author.name}</span>
+              </section>
+                <button 
+                  className={` py-1 px-3 rounded ${author.isFollowing ? 'bg-red-500 text-white': 'bg-black text-white'}`}
+                  onClick={() => handleFollowClick(index)}
+                >
+                  {author.isFollowing ? 'Unfollow' : 'Follow'}
+                </button>
+            </li>
+          ))}
+        </ul>
     </div>
   )
 }
